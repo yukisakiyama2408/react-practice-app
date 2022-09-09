696487df4950b82cc76921ae71b2b9d4d3138666
Sakiyama Yuki <yuki.king.0422@gmail.com>
2022-09-09 20:30:55 +0900

    update

---
+++ src/Sample.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Sample = () => {
+  return (
+    <>
+      <div>
+        <h2>自己紹介</h2>
+        <p>こんにちは、崎山です。</p>
+      </div>
+    </>
+  );
+};
+
+export default Sample;
